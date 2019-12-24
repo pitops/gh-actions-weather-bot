d03e6c6cf8d0a0c5ab11876c9a91015e252091e7
Petros Kyriakou <9pitops@gmail.com>
2019-12-24 18:05:30 +0200

    refactor, ability to also display for multiple cities

--- index.js
+++ index.js
@@ -1,46 +1,69 @@
 require('dotenv').config()
 
 const fetch = require('node-fetch')
-const weatherToken = process.env.ACCUWEATHER_API_KEY
 const Slack = require('slack-node')
+const util = require('util')
 
+const weatherToken = process.env.ACCUWEATHER_API_KEY
 const slackWebhookSecret = process.env.SLACK_WEBHOOK_SECRET
+
+const CITIES = [
+  {
+    name: 'Limassol',
+    locationKey: '124034'
+  },
+  {
+    name: 'Abu Dhabi',
+    locationKey: '321626'
+  }
+]
 
 const slack = new Slack()
 slack.setWebhook(`https://hooks.slack.com/services/${slackWebhookSecret}`)
 
-const limassolLocationKey = process.env.LIMASSOL_LOCATION_KEY
+// Promisify the slack webhook method
+const slackWebhook = util.promisify(slack.webhook)
 
-const weatherURL = new URL(
-  `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${limassolLocationKey}`
-)
-weatherURL.searchParams.set('apikey', weatherToken)
-weatherURL.searchParams.set('metric', true)
+const weatherURL = locationKey => {
+  const url = new URL(
+    `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${locationKey}`
+  )
 
-const getWeatherData = async () => {
-  const resp = await fetch(weatherURL.toString())
+  url.searchParams.set('apikey', weatherToken)
+  url.searchParams.set('metric', true)
+
+  return url
+}
+
+const getWeatherData = async locationKey => {
+  const resp = await fetch(weatherURL(locationKey).toString())
   const body = await resp.json()
   return body
 }
 
-const generateWeatherMessage = weatherData =>
-  `The weather in Limassol: ${weatherData.Headline.Text}. Highest temperature will be ${weatherData.DailyForecasts[0].Temperature.Maximum.Value} °C, and lowest temperature will be ${weatherData.DailyForecasts[0].Temperature.Minimum.Value} °C`
+const generateWeatherMessage = (weatherData, city) =>
+  `The weather in *${city}*: ${weatherData.Headline.Text}. Highest temperature will be *${weatherData.DailyForecasts[0].Temperature.Maximum.Value}* °C, and lowest temperature will be *${weatherData.DailyForecasts[0].Temperature.Minimum.Value}* °C`
 
-const postToSlack = message => {
-  slack.webhook(
-    {
-      channel: '#general',
-      username: 'Kokoras',
-      text: message
-    },
-    (error, response) => console.log(error)
-  )
+const postToSlack = async message => {
+  await slackWebhook({
+    channel: '#general',
+    username: 'Kokoras',
+    text: message
+  })
 }
 
 const main = async () => {
-  const weatherData = await getWeatherData()
-  const weatherString = generateWeatherMessage(weatherData)
-  postToSlack(weatherString)
+  for (let i = 0; i < CITIES.length; i++) {
+    const { name, locationKey } = CITIES[i]
+
+    try {
+      const weatherData = await getWeatherData(locationKey)
+      const weatherString = generateWeatherMessage(weatherData, name)
+      await postToSlack(weatherString)
+    } catch (err) {
+      await postToSlack(`Unable to get weather data. Error: ${err.message}`)
+    }
+  }
 }
 
 main()
